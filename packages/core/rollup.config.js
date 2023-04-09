49f1e1bff60194404ba0e0b6bfe4080563c0a45a
destiny <1848765519@qq.com>
2023-04-09 15:14:24 +0800

    feat(core): init core package

---
+++ packages/core/rollup.config.js
@@ -0,0 +1,16 @@
+import nodeResolve from '@rollup/plugin-node-resolve';
+import commonjs from '@rollup/plugin-commonjs';
+import typescript from '@rollup/plugin-typescript';
+import json from '@rollup/plugin-json';
+
+export default {
+  input: {
+    index: 'src/index.ts',
+  },
+  output: {
+    dir: 'lib',
+    format: 'es',
+  },
+  external: [/node_modules/],
+  plugins: [nodeResolve(), commonjs(), typescript(), json()],
+};
